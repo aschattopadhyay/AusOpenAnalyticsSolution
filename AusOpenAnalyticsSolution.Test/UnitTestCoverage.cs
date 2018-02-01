c66c7e36a0a477d857fc2a2960f5d6fba3a93fb9
aschattopadhyay@gmail.com <aschattopadhyay@gmail.com>
2018-02-02 09:46:31 +1100

    Changed Unit test to xUnit test

--- AusOpenAnalyticsSolution.Test/UnitTestCoverage.cs
+++ AusOpenAnalyticsSolution.Test/UnitTestCoverage.cs
@@ -1,43 +1,13 @@
-using AusOpenAnalyticsSolution.BO;
-using AusOpenAnalyticsSolution.Utility;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
+using System;
+using Xunit;
 
 namespace AusOpenAnalyticsSolution.Test
 {
-    [TestClass]
     public class UnitTestCoverage
     {
-      
-        [TestMethod]
-        public void TestGetPlayerOne()
+        [Fact]
+        public void Test1()
         {
-
-            CsvRecord csvRecord = new CsvRecord();
-            csvRecord.PlayerOneName = "Test1";
-            csvRecord.PlayerTwoName = "Test2";
-            csvRecord.PlayerOnePoint = "1";
-            csvRecord.PlayerTwoPoint = "2";
-            csvRecord.PlayerOneAce = "TRUE";
-            csvRecord.PlayerTwoAce = "FALSE";
-            csvRecord.PlayerOneGame = "1";
-            csvRecord.PlayerTwoGame = "2";
-            csvRecord.PlayerOneSet = "1";
-            csvRecord.PlayerTwoSet = "2";
-            csvRecord.PlayerOneFirstServe = "4d";
-            csvRecord.PlayerOneFirstServeIn = "yes";
-            csvRecord.PlayerTwoFirstServe = "4b";
-            csvRecord.PlayerTwoFirstServeIn = "no";
-            csvRecord.PlayerOneFirstServeShot = "4";
-            csvRecord.PlayerOneSecondServeShot = "6";
-            csvRecord.PlayerTwoFirstServeShot = "5";
-            csvRecord.PlayerTwoSecondServeShot = "5";
-            csvRecord.PlayerOneServeDirection = "L";
-            csvRecord.PlayerTwoServeDirection = "";
-            csvRecord.ServeDirectionDecider = "0-0";
-            Match testMatch = new Match(1);
-            var result = testMatch.GetPlayerOne(csvRecord, null);
-            Assert.IsNotNull(result);
 
         }
     }
